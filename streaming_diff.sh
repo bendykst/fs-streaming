99cf712aafbed66f589118d9191c3fb4fb4d5b86
Benjamin Dykstra <bendykst@gmail.com>
2016-04-03 10:17:56 -0500

    Updated streaming_diff.sh documentation

--- streaming_diff.sh
+++ streaming_diff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Diff the output of two streamable.clj outputs.
+# Diff the output of two streamable.clj runs for a specific streaming source
 # Usage: streaming_diff.sh source_tag file1 file2
 colordiff \
   <(grep "\*\*$1\*\*" $2 | sed 's/|.*$//') \
